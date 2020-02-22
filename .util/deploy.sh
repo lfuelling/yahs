1499ac60635da5b80fe81180acb414c3f239b7f8
Lukas Fülling <lukas@k40s.net>
2020-02-22 03:13:59 +0100

    fix deploy

---
+++ .util/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_REPO_SLUG" == "lfuelling/yahs" ]]; then
+  echo -e "Deploying package..."
+  mvn deploy -Dregistry=https://maven.pkg.github.com/lfuelling -Dtoken="${GH_TOKEN}"
+  echo -e "Deployed package."
+fi
